3359ea4a15f64b07f16bd8df8ee863b0e49c8487
7ydsm7 <piotrochyra2002@gmail.com>
2023-05-12 09:15:27 +0200

    Uporządkowana wersja

--- sgh.java
+++ sgh.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class sgh {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System. in);
@@ -12,51 +11,64 @@ public class sgh {
 
         System.out.println("Podaj ilość lat: ");
         int liczbaLat = scanner.nextInt();
-
-        //dla wszystkich rodzaji kapitalizacji
       
-       
-        //odnośnik do wzoru dla opcji kwartalnej
-        int iloscKwartalow = liczbaLat*4;
-        double stopaKwartalna = oprocentowanieRoczne / 4;
-
-        //odnośnik dla opcji miesięcznej
-        int iloscMiesiecy = liczbaLat*12;
-        double stopaMiesieczna = oprocentowanieRoczne / 12;      
-        
+        int wybor = 0;
+        double kapital = 0;
+        while(true) {
+            
             System.out.println("Podaj spsób kapitalizacji odsetek: ");
             System.out.println("1. Odsetki roczne");
             System.out.println("2.Odsetki kwartalne");
             System.out.println("3.Odsetki miesieczne");
-           
+            System.out.println("0.Wyjdź");
+            wybor = scanner.nextInt();
 
-            int wybor = scanner.nextInt();
-            
+            if (wybor == 0) {
+                System.out.println("Zamkyanie apliakcji...");
+                break;
+            }
+            else if (wybor < 1 || wybor > 3) {
+                System.out.println("Wybierz opcję 1-3!");
+                continue;
+            }
+
+
+       
           
-
         switch (wybor){
  case 1:
 
-             // odsetki składane rocznie 
-            double kapital = poczatkowaWartosc * Math.pow(1 + oprocentowanieRoczne, liczbaLat);
-            System.out.println("Po " + liczbaLat + " latach zarobisz: " + kapital);
-             break;
+        kapital = odsetkiRoczne(poczatkowaWartosc, oprocentowanieRoczne, liczbaLat);
+        break;
  case 2:
-        //odsetki kwartalne    
-            
-             kapital = poczatkowaWartosc * Math.pow(1 + stopaKwartalna, iloscKwartalow);
-            System.out.println("Po " + liczbaLat + " latach zarobisz: " + kapital);
-            break;
 
-        
- case 3:
-        //odsetki miesięczne 
-         kapital = poczatkowaWartosc * Math.pow(1 + stopaMiesieczna, iloscMiesiecy);
-        System.out.println("Po " + liczbaLat + " latach zarobisz: " + kapital);
+        kapital = odsetkiKwartalne(poczatkowaWartosc, oprocentowanieRoczne, liczbaLat);    
+        break;
+case 3:
+        kapital = odsetkiRoczne(poczatkowaWartosc, oprocentowanieRoczne, liczbaLat);        
         break;  
-        default:
-                System.out.println("Nieprawidłowy wybór.");
-                break;
         }
+        System.out.println("Po " + liczbaLat + " latach zarobisz: " + kapital);
     }  
+        }
+           
+   
+    public static double odsetkiRoczne(double poczatkowaWartosc, double oprocentowanieRoczne, int liczbaLat) {
+    double kapital = poczatkowaWartosc * Math.pow(1 + oprocentowanieRoczne, liczbaLat);
+    return kapital;
+    }
+   
+    public static double odsetkiKwartalne(double poczatkowaWartosc, double oprocentowanieRoczne, int liczbaLat) {
+        int iloscKwartalow = liczbaLat*4;
+        double stopaKwartalna = oprocentowanieRoczne / 4;
+        double kapital = poczatkowaWartosc * Math.pow(1 + stopaKwartalna, iloscKwartalow);
+        return kapital;
+    }
+    
+    public static double odsetkiMiesieczne(double poczatkowaWartosc, double oprocentowanieRoczne, int liczbaLat) {
+        int iloscMiesiecy = liczbaLat*12;
+        double stopaMiesieczna = oprocentowanieRoczne / 12; 
+        double kapital = poczatkowaWartosc * Math.pow(1 + stopaMiesieczna, iloscMiesiecy);
+        return kapital;
+    }
 }
